929317566834f943fcadc1162f06f70375beff90
chocolatl <choclatl@outlook.com>
2017-06-30 20:44:24 +0800

    修改日志逻辑
    生产环境中会详细记录日志，并将日志输出到access.log中

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require('fs');
 
 var index = require('./routes/index');
 
@@ -15,7 +16,14 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+if(app.get('env') === 'development') {
+  app.use(logger('dev'));
+} else {
+  // create a write stream (in append mode)
+  let accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
+  let logFormat = '[:remote-addr] :method :status :url [:response-time[0] ms] (:date[iso]) \\r\\n:user-agent\\r\\n\\r\\n';
+  app.use(logger(logFormat, {stream: accessLogStream}));
+}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
