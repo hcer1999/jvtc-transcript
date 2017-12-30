f668cdac5fcc433d233f28e41946fadb249d31af
chocolatl <choclatl@outlook.com>
2017-12-30 15:16:48 +0800

    添加compression模块压缩传输体积

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var compression = require('compression');
+var zlib = require('zlib');
 var fs = require('fs');
 
 var index = require('./routes/index');
@@ -14,6 +16,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('view cache', false);
+
+app.use(compression({level: zlib.Z_BEST_COMPRESSION}));
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
