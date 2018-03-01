b3765581cd900f456de78bf303fe7d91c25bc298
chocolatl <choclatl@outlook.com>
2018-03-01 22:59:44 +0800

    添加后端识别验证码的功能
    添加后端识别验证码的功能，当前端提交的表单验证码字段为空的时候，在后端获取验证码并识别

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,18 @@ const path = require('path');
 const Cache = require('../lib/Cache');
 const User = require('../lib/jwweb');
 const logUser = require('../lib/log-user')(path.join(__dirname, '..', 'public', 'user.log'));
+const Captcha = require('../lib/Captcha');
+const Jimp = require("jimp");
+const sampleData = require('../lib/sample-data');
+const Recognizer = require('../lib/Recognizer');
+
+const recognizer = new Recognizer();
+
+// 加载样本数据
+for(let {char, sampleVal} of sampleData) {
+    recognizer.addSampleData(char, sampleVal);
+}
+
 
 const userCache = new Cache(10 * 60 * 1000);    // 用户会话记录缓存十分钟
 
@@ -36,11 +48,37 @@ router.get('/', function(req, res, next) {
     }).catch(next);
 });
 
+async function fillCaptcha(user) {
+    let code = '';
+    while(1) {
+        let imageData = await user.getCaptcha();
+        let captcha = await new Captcha(imageData);
+        let slicedChars = await captcha.dispose().sliceCharacter();
+        if(slicedChars.length !== 4) {
+            continue;
+        }
+        for(let slicedChar of slicedChars) {
+            let sampleString = slicedChar.resize(24, 24).getSampleString();
+            let matchedChar = recognizer.matchSample(sampleString);
+            code += matchedChar;
+        }
+        break;
+    }
+    return code;
+}
+
 router.post('/', function(req, res, next) {
     if(!req.user) throw new Error('UID Not Exist');
     
     let form = req.body;
-    req.user.login(form).then(logined => {
+
+    // 提交的表单captcha字段为空时，在后端获取验证码识别
+    let getCaptcha = form.captcha ? Promise.resolve(form.captcha) : fillCaptcha(req.user);
+
+    getCaptcha.then(code => {
+        form.captcha = code;
+        return req.user.login(form);
+    }).then(logined => {
         if(!logined) throw new Error('Login Failed');
         res.redirect(303, `/transcript/${form.userid}/${form.semester}`);
     }).catch(next);
