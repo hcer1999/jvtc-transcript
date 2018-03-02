c5293805f8b483a398937b7f75535dad8c5b21ff
chocolatl <choclatl@outlook.com>
2018-03-02 16:05:51 +0800

    双引号换为单引号

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const User = require('../lib/jwweb');
 const Log = require('../lib/Log');
 const actionLog = new Log((path.join(__dirname, '..', 'public', 'action.log')));
 const Captcha = require('../lib/Captcha');
-const Jimp = require("jimp");
+const Jimp = require('jimp');
 const sampleData = require('../lib/sample-data');
 const Recognizer = require('../lib/Recognizer');
 
